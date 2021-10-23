80cb4cdb71aeda0d2275eed3a537da55833ac8fa
LukaSesar <luka.sesar99@gmail.com>
2021-10-23 17:52:51 +0200

    Navbar added

--- src/web/Main.js
+++ src/web/Main.js
@@ -1,10 +1,18 @@
 import React from "react";
+import Navbar from "./navbar/Navbar";
+import "./style.scss";
 
 const Main = () => {
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
+    <div className="main">
+      <Navbar />
+      <div className="main-header">
+        <p>
+          Hi, my name is <span className="my-name">Luka Sesar.</span>
+        </p>
+        <p>I'm a front-end web developer.</p>
+      </div>
+    </div>
   );
 };
 
---
+++ src/web/navbar/Navbar.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { FaBars } from "react-icons/fa";
+import { Link } from "react-scroll";
+import "./nav.scss";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="nav-cont">
+        <div className="mobile-icon">
+          <FaBars />
+        </div>
+        <ul className="nav-menu">
+          <li>
+            <Link className="nav-link" to="home" spy={true} smooth={true}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link className="nav-link" to="about" spy={true} smooth={true}>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link className="nav-link" to="portfolio" spy={true} smooth={true}>
+              Portfolio
+            </Link>
+          </li>
+          <li>
+            <Link className="nav-link" to="Contact" spy={true} smooth={true}>
+              Contact
+            </Link>
+          </li>
+        </ul>
+        <div className="nav-btn">
+          <a
+            className="nav-btn-link"
+            href="https://github.com/sesar002"
+            target="a_blank"
+          >
+            GitHub
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
